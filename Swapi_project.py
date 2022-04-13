7a79f3d8a3b5065d715e792f6a5924394629eb29
Avinash Prakash <aprakash@spartaglobal.com>
2022-04-13 18:16:58 +0100

    Update

--- Swapi_project.py
+++ Swapi_project.py
@@ -6,28 +6,32 @@ client = pymongo.MongoClient()
 db = client["starwars"]
 
 def swapi(url):
+    # Creating a list of starships
     list= []
 
     response = requests.get(url)
     data = response.json()
-    # pprint(data)
 
+    # Storing the first page of results
     list += data["results"]
 
     while data["next"] is not None:
-
         response = requests.get(data["next"])
         data = response.json()
-
+        # Storing the current page of results
         list += data["results"]
     return list
 
+# Returns all starships from each page
 starship_list = swapi('https://swapi.dev/api/starships/')
 
+# Drops the previous starships collection
 db.starships.drop()
 
+# Looping over starship_list for starships that have pilots
 for i in starship_list:
     if len(i["pilots"]) > 0:
+        # Replacing API urls with ObjectIds from characters collection in mongodb
         for index, pilot in enumerate(i["pilots"]):
             pilot_info = requests.get(pilot).json()
             pilot_name = pilot_info["name"]
